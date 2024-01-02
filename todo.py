5690ddef201ae98bd9055b2d8a99d9d6296ddd4e
nikitaNotvas <nvasiuta@brooklynsteamcenter.org>
2024-01-02 10:50:52 -0500

    errors are funny

--- todo.py
+++ todo.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
 
 app = Flask(__name__)
 
@@ -8,11 +8,15 @@ todolist=['Get to plat in overwatch 2 in tank or dps','Steal THE moon']
 
 @app.route('/',methods=["GET","POST"])
 def index():
-    new_todo = request.form["new_todo"]
-    todolist.append(new_todo)
+    if request.method == "POST":
+        new_todo = request.form["new_todo"]
+        todolist.append(new_todo)
+
     return render_template("todoweb.html.jinja",todolist=todolist)
     
-
-
-
+@app.route('/delete_todo/<int:todo_index>',methods=["POST"])
+def todo_delete(todo_index):
+    
+    del todolist[todo_index]
+    return redirect('/')
 
